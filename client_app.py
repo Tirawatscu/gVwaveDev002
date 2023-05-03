0e69ec19305cb6fc43ef560f8bbaa329064a072d
tirawatscu <tirawat.scu97@gmail.com>
2023-05-03 19:14:56 +0700

    Hot

--- client_app.py
+++ client_app.py
@@ -2,9 +2,10 @@ import socket
 from struct import unpack, pack
 import random
 
-server_ip = '192.168.1.106'
-server_port = 65000
-server_address = (server_ip, server_port)
+server_ip = '192.168.1.106'  # Replace with the server's IP address
+host, port = server_ip, 65000
+server_address = (host, port)
+
 
 def receive_start_signal(sock):
     data = b''
@@ -15,28 +16,30 @@ def receive_start_signal(sock):
 
 
 def send_samples(sock, num_samples):
+    x, y, z = random.random(), random.random(), random.random()
+
     for i in range(num_samples):
-        x, y, z = random.random(), random.random(), random.random()
         message = pack('3f', x, y, z)
-        print(f"Sending sample {i+1}: X: {x}, Y: {y}, Z: {z}")
+        print(f"Sending sample {i + 1}: X: {x}, Y: {y}, Z: {z}")
         sock.sendall(message)
+
         x += 1
         y += 1
         z += 1
+
 
 def client_listen():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(server_address)
 
-    while True:
+    try:
         num_samples, start_signal = receive_start_signal(sock)
-        if start_signal:
+        if start_signal == 1:
             print(f"Received start signal with {num_samples} samples")
             send_samples(sock, num_samples)
-        else:
-            print("No start signal received. Waiting...")
+    finally:
+        sock.close()
 
-    sock.close()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     client_listen()
