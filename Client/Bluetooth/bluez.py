547f56b9c096395bc0c2c2df4072dde91ed44ded
tirawatscu <tirawat.scu97@gmail.com>
2023-06-01 00:50:24 +0700

    String

--- Client/Bluetooth/bluez.py
+++ Client/Bluetooth/bluez.py
@@ -25,10 +25,12 @@ def listen_for_connections():
             # Send a response
             data_str = data.decode("utf-8").strip()
             if data_str == "Start":
-                floats_to_send = [1.1, 2.2, 3.3]
-                byte_data = b''
-                for float_val in floats_to_send:
-                    byte_data += struct.pack('f', float_val)
+                floats_to_send = [1.1123, 2.2231, 3.3644]
+                # Convert each float to string and join them into a single string
+                string_data = ','.join(map(str, floats_to_send))
+
+                # Convert the string to bytes
+                byte_data = string_data.encode('utf-8')
                 
                 # Now pack this uint8 list to bytes
                 response = byte_data
